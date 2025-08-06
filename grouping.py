9f09f7ea4bbdf00cc334f494a165cad32e00bd51
sample tarou <sample@example.com>
2025-08-06 20:13:56 +0900

    amend

--- grouping.py
+++ grouping.py
@@ -1,16 +1,12 @@
 import random
 
 #A~Fまでの６人がいるリストを用意する
-gr = ['A', 'B', 'C', 'D', 'E', 'F']
+friends = ['A', 'B', 'C', 'D', 'E', 'F']
 #リストから3 人、または 2 人をランダムに取り出す
-n = random.choice([2, 3])
-r = random.sample(gr, n)
-#２人が取り出された場合、他の４人を取り出す
-if n == 2:
-    others = [p for p in gr if p not in r]
-#２人以外が取り出された場合、他の３人を取り出す
-else:
-    others = [p for p in gr if p not in r]
+group_choice = random.choice([2, 3])
+group_first = random.sample(friends, group_choice)
+# 上のグループに入らなかった人を洗い出して別のグループに全員を入れる
+group_second = [p for p in friends if p not in group_first]
 #グループごとに分けて出力する
-print(r)
-print(others)
+print(group_first)
+print(group_second)
